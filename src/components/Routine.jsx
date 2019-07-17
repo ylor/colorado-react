255b03d138fd90a183447f5076fe3d393f1354af
Roly <rolyreyes@me.com>
2019-07-17 15:15:53 -0400

    second commit

---
+++ src/components/Routine.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import data from "../data/routine.json";
+
+export default function Routine() {
+  const [exercises, setExercises] = data;
+  console.log(exercises);
+  return <h1>asdf</h1>;
+}
